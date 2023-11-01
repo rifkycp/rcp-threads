5bfb3823579b65a158b47017cac7879c46f3a41b
Rifky <rifkycp@gmail.com>
2023-11-01 17:37:55 +0700

    repair onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,14 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
     const user = await currentUser();
-    const userInfo = {};
+    if (!user) return null;
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
